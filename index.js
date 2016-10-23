fbcc190ef45162d93617283b696772ed4d8dee85
natsu12 <natsu12@126.com>
2016-10-23 13:37:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+function IsPC()  
+{  
+    var userAgentInfo = navigator.userAgent;  
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+    var flag = true;  
+    for (var v = 0; v < Agents.length; v++) {  
+       if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }  
+    }  
+    return flag;  
+} 
+
+$(function  () {
+  var times = 0;
+  var onclickEvent = function() {
+    times++;
+
+    if (times >= 1000) {
+      $('.result').text('恭喜你已完成，请截图带面板私聊发给主板');
+    } else {
+      $('.result').text('你已点击'+times+'次');
+
+    }
+
+  }
+
+  if (IsPC()) {
+    $('.test')[0].addEventListener("click", onclickEvent, false);
+  } else {
+    $('.test')[0].addEventListener("touchstart", onclickEvent, false);
+  }
+})
